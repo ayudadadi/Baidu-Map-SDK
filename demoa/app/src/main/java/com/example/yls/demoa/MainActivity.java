b787935a0e7186628949fbfd753ae979cb8ce445
ayudadadi <ayuisnice@outlook.com>
2019-04-03 15:53:15 +0800

    百度地图SDK Android开发

---
+++ demoa/app/src/main/java/com/example/yls/demoa/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.yls.demoa;
+
+import android.location.Location;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+
+public class MainActivity extends AppCompatActivity {
+    private MapView mMapView = null;
+    private BaiduMap mBaiduMap;
+    private LocationClient mLocationClient;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMapView = (MapView) findViewById(R.id.bmapView);
+        mBaiduMap = mMapView.getMap();
+//普通地图 ,mBaiduMap是地图控制器对象
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+        mBaiduMap.setMyLocationEnabled(true);
+
+        MyLocationConfiguration myLocationConiguration = new MyLocationConfiguration(
+                MyLocationConfiguration.LocationMode.FOLLOWING,
+        true,
+                BitmapDescriptorFactory.fromResource(R.drawable.tubiao),
+        0xAAFFFF88,
+        0xAA00FF00);
+
+        mBaiduMap.setMyLocationConfiguration(myLocationConiguration);
+        //定位初始化
+        mLocationClient = new LocationClient(this);
+
+//通过LocationClientOption设置LocationClient相关参数
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setScanSpan(1000);
+
+//设置locationClientOption
+        mLocationClient.setLocOption(option);
+
+//注册LocationListener监听器
+        MyLocationListener myLocationListener = new MyLocationListener();
+        mLocationClient.registerLocationListener(myLocationListener);
+//开启地图定位图层
+        mLocationClient.start();
+    }
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        mLocationClient.stop();
+        mBaiduMap.setMyLocationEnabled(false);
+        mMapView.onDestroy();
+        mMapView = null;
+        super.onDestroy();
+    }
+    public class MyLocationListener extends BDAbstractLocationListener {
+        public void onReceiveLocation(BDLocation location) {
+            //mapView 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null){
+                return;
+            }
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(location.getDirection()).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+        }
+    }
+}
